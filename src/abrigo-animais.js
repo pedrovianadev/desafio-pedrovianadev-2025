e2ea18619ce890abc4129123a0d0916148aad063
Pedro Viana <pedroviana.developer13@gmail.com>
2025-09-07 20:16:55 -0300

    feat: implementa estrutura básica e validações de entrada

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,6 +1,16 @@
 class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    // Definindo os dados dos animais
+    const animais = {
+      'Rex': { tipo: 'cão', brinquedos: ['RATO', 'BOLA'] },
+      'Mimi': { tipo: 'gato', brinquedos: ['BOLA', 'LASER'] },
+      'Fofo': { tipo: 'gato', brinquedos: ['BOLA', 'RATO', 'LASER'] },
+      'Zero': { tipo: 'gato', brinquedos: ['RATO', 'BOLA'] },
+      'Bola': { tipo: 'cão', brinquedos: ['CAIXA', 'NOVELO'] },
+      'Bebe': { tipo: 'cão', brinquedos: ['LASER', 'RATO', 'BOLA'] },
+      'Loco': { tipo: 'jabuti', brinquedos: ['SKATE', 'RATO'] }
+    };
   }
 }
 
